e5a5905ddf02de8fd3e50f424258f578915b2b2c
Aizat <aizat.temirbekkyzy@iaau.edu.kg>
2016-12-17 14:36:54 +0600

    function

---
+++ ru/js/functions.js
@@ -0,0 +1,19 @@
+
+function changeLang(lang){
+	var arr = document.location.pathname.split("/");
+	arr[2] = lang;
+	document.cookie = "lang=ru;path=/";
+	window.location.assign(pathToString(arr));
+
+}
+
+function pathToString(path){
+	var p = "";
+	for(i=0;i<path.length;i++){
+		p += path[i];
+		if(i<path.length-1){
+			p+="/";
+		}
+	}
+	return p;
+}
