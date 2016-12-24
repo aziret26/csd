95afda7ba05479d6433efa9ffc860af3c99c5a2d
aizat09 <aizat.temirbekkyzy@iaau.edu.kg>
2016-12-24 16:10:27 +0600

    Update script.js
    imporved header behaviour

--- en/js/script.js
+++ en/js/script.js
@@ -1,5 +1,21 @@
 $(window).scroll(function() {
-	if($(this).scrollTop() > 1){
+	if($(this).width()<767){
+		$(".csd-text").addClass("csd-text-hidden");
+		$(".csd-text-hidden").removeClass("csd-text");
+	}else
+	{
+		if($(this).scrollTop() > 1){
+			$(".csd-text").addClass("csd-text-hidden");
+			$(".csd-text-hidden").removeClass("csd-text");
+		}else{
+			$(".csd-text-hidden").addClass("csd-text");
+			$(".csd-text").removeClass("csd-text-hidden");
+		}
+	}
+});
+
+$(document).ready(function(){
+	if($(this).width()<767){
 		$(".csd-text").addClass("csd-text-hidden");
 		$(".csd-text-hidden").removeClass("csd-text");
 	}else
@@ -8,3 +24,4 @@ $(window).scroll(function() {
 		$(".csd-text").removeClass("csd-text-hidden");
 	}
 });
+
